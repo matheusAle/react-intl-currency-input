4592e893c3b3af6a06726122d2721a4f5769ab7e
Matheus Ale da Silva <m.matheus.ale@gmail.com>
2021-01-23 18:34:43 +0000

    forward ref

--- src/index.js
+++ src/index.js
@@ -4,193 +4,215 @@ import { string, func, number, bool, shape, node } from 'prop-types';
 import formatCurrency from './format-currency';
 
 const defaultConfig = {
-  locale: 'en-US',
-  formats: {
-    number: {
-      USD: {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-      },
+    locale: 'en-US',
+    formats: {
+        number: {
+            USD: {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+            },
+        },
     },
-  },
 };
 
-const IntlCurrencyInput = ({
-  component: InputComponent,
-  value,
-  defaultValue,
-  config,
-  currency,
-  max,
-  autoFocus,
-  autoSelect,
-  autoReset,
-  onChange,
-  onBlur,
-  onFocus,
-  onKeyPress,
-  ...otherProps
-}) => {
-  const inputRef = useCallback(node => {
+function useCombinedRefs(...refs) {
+    const targetRef = React.useRef()
+
+    React.useEffect(() => {
+        refs.forEach(ref => {
+            if (!ref) return
+
+            if (typeof ref === 'function') {
+                ref(targetRef.current)
+            } else {
+                ref.current = targetRef.current
+            }
+        })
+    }, [refs])
+
+    return targetRef
+}
+
+
+const IntlCurrencyInput = React.forwardRef(({
+    component: InputComponent,
+    value,
+    defaultValue,
+    config,
+    currency,
+    max,
+    autoFocus,
+    autoSelect,
+    autoReset,
+    onChange,
+    onBlur,
+    onFocus,
+    onKeyPress,
+    ...otherProps
+}, ref) => {
     const isActive = node === document.activeElement;
+    const inputRef = useCallback(node => {
 
-    if (node && autoFocus && !isActive) {
-      node.focus();
-    }
-  }, [autoFocus]);
+        if (node && autoFocus && !isActive) {
+            node.focus();
+        }
+    }, [autoFocus]);
 
-  const [maskedValue, setMaskedValue] = useState('0');
+    const innerRef = useCombinedRefs(innerRef, ref)
 
-  // to prevent a malformed config object
-  const safeConfig = useMemo(() => () => {
-    const { formats: { number: { [currency]: { maximumFractionDigits } } } } = config;
 
-    const finalConfig = {
-      ...defaultConfig,
-      ...config,
+    const [maskedValue, setMaskedValue] = useState('0');
+
+    // to prevent a malformed config object
+    const safeConfig = useMemo(() => () => {
+        const { formats: { number: { [currency]: { maximumFractionDigits } } } } = config;
+
+        const finalConfig = {
+            ...defaultConfig,
+            ...config,
+        };
+
+        // at the moment this prevents problems when converting numbers
+        // with zeroes in-between, otherwise 205 would convert to 25.
+        finalConfig.formats.number[currency].minimumFractionDigits = maximumFractionDigits;
+
+        return finalConfig;
+    }, [defaultConfig, config]);
+
+    const clean = (number) => {
+        if (typeof number === 'number') {
+            return number;
+        }
+
+        // strips everything that is not a number (positive or negative)
+        return Number(number.toString().replace(/[^0-9-]/g, ''));
     };
 
-    // at the moment this prevents problems when converting numbers
-    // with zeroes in-between, otherwise 205 would convert to 25.
-    finalConfig.formats.number[currency].minimumFractionDigits = maximumFractionDigits;
+    const normalizeValue = number => {
+        const { formats: { number: { [currency]: { maximumFractionDigits } } } } = safeConfig();
+        let safeNumber = number;
 
-    return finalConfig;
-  }, [defaultConfig, config]);
+        if (typeof number === 'string') {
+            safeNumber = clean(number);
 
-  const clean = (number) => {
-    if (typeof number === 'number') {
-      return number;
-    }
-    
-    // strips everything that is not a number (positive or negative)
-    return Number(number.toString().replace(/[^0-9-]/g, ''));
-  };
+            if (safeNumber % 1 !== 0) {
+                safeNumber = safeNumber.toFixed(maximumFractionDigits);
+            }
 
-  const normalizeValue = number => {
-    const { formats: { number: { [currency]: { maximumFractionDigits } } } } = safeConfig();
-    let safeNumber = number;
+        } else {
+            // all input numbers must be a float point (for the cents portion). This is a fallback in case of integer ones.
+            safeNumber = Number.isInteger(number) ? Number(number) * (10 ** maximumFractionDigits) : number.toFixed(maximumFractionDigits);
+        }
 
-    if (typeof number === 'string') {
-      safeNumber = clean(number);
+        // divide it by 10 power the maximum fraction digits.
+        return clean(safeNumber) / (10 ** maximumFractionDigits);
+    };
 
-      if (safeNumber % 1 !== 0) {
-        safeNumber = safeNumber.toFixed(maximumFractionDigits);
-      }
+    const calculateValues = (inputFieldValue) => {
+        const value = normalizeValue(inputFieldValue);
+        const maskedValue = formatCurrency(value, safeConfig(), currency);
 
-    } else {
-      // all input numbers must be a float point (for the cents portion). This is a fallback in case of integer ones.
-      safeNumber = Number.isInteger(number) ? Number(number) * (10 ** maximumFractionDigits) : number.toFixed(maximumFractionDigits);
-    }
-    
-    // divide it by 10 power the maximum fraction digits.
-    return clean(safeNumber) / (10 ** maximumFractionDigits);
-  };
+        return [value, maskedValue];
+    };
 
-  const calculateValues = (inputFieldValue) => {
-    const value = normalizeValue(inputFieldValue);
-    const maskedValue = formatCurrency(value, safeConfig(), currency);
+    const updateValues = (value) => {
+        const [calculatedValue, calculatedMaskedValue] = calculateValues(value);
 
-    return [value, maskedValue];
-  };
+        if (!max || calculatedValue <= max) {
+            setMaskedValue(calculatedMaskedValue);
 
-  const updateValues = (value) => {
-    const [calculatedValue, calculatedMaskedValue] = calculateValues(value);
-    
-    if (!max || calculatedValue <= max) {
-      setMaskedValue(calculatedMaskedValue);
+            return [calculatedValue, calculatedMaskedValue];
+        } else {
+            return [normalizeValue(maskedValue), maskedValue];
+        }
+    };
 
-      return [calculatedValue, calculatedMaskedValue];
-    } else {
-      return [normalizeValue(maskedValue), maskedValue];
-    }
-  };
+    const handleChange = (event) => {
+        event.preventDefault();
 
-  const handleChange = (event) => {
-    event.preventDefault();
+        const [value, maskedValue] = updateValues(event.target.value);
 
-    const [value, maskedValue] = updateValues(event.target.value);
+        if (maskedValue) {
+            onChange(event, value, maskedValue);
+        }
+    };
 
-    if (maskedValue) {
-      onChange(event, value, maskedValue);
-    }
-  };
+    const handleBlur = (event) => {
+        const [value, maskedValue] = updateValues(event.target.value);
 
-  const handleBlur = (event) => {
-    const [value, maskedValue] = updateValues(event.target.value);
+        if (autoReset) {
+            calculateValues(0);
+        }
 
-    if (autoReset) {
-      calculateValues(0);
-    }
+        if (maskedValue) {
+            onBlur(event, value, maskedValue);
+        }
+    };
 
-    if (maskedValue) {
-      onBlur(event, value, maskedValue);
-    }
-  };
+    const handleFocus = (event) => {
+        if (autoSelect) {
+            event.target.select();
+        }
 
-  const handleFocus = (event) => {
-    if (autoSelect) {
-      event.target.select();
-    }
+        const [value, maskedValue] = updateValues(event.target.value);
 
-    const [value, maskedValue] = updateValues(event.target.value);
+        if (maskedValue) {
+            onFocus(event, value, maskedValue);
+        }
+    };
 
-    if (maskedValue) {
-      onFocus(event, value, maskedValue);
-    }
-  };
+    const handleKeyUp = event => onKeyPress(event, event.key, event.keyCode);
 
-  const handleKeyUp = event => onKeyPress(event, event.key, event.keyCode);
+    useEffect(() => {
+        const currentValue = value || defaultValue || 0;
+        const [, maskedValue] = calculateValues(currentValue);
 
-  useEffect(() => {
-    const currentValue = value || defaultValue || 0;
-    const [, maskedValue] = calculateValues(currentValue);
+        setMaskedValue(maskedValue);
+    }, [currency, value, defaultValue, config]);
 
-    setMaskedValue(maskedValue);
-  }, [currency, value, defaultValue, config]);
-
-  return (
-    <InputComponent
-      {...otherProps}
-      ref={inputRef}
-      value={maskedValue}
-      onChange={handleChange}
-      onBlur={handleBlur}
-      onFocus={handleFocus}
-      onKeyUp={handleKeyUp}
-    />
-  );
-};
+    return (
+        <InputComponent
+            {...otherProps}
+            ref={innerRef}
+            value={maskedValue}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            onFocus={handleFocus}
+            onKeyUp={handleKeyUp}
+        />
+    );
+});
 
 IntlCurrencyInput.propTypes = {
-  defaultValue: number,
-  value: number,
-  max: number,
-  component: node.isRequired,
-  currency: string.isRequired,
-  config: shape().isRequired,
-  autoFocus: bool.isRequired,
-  autoSelect: bool.isRequired,
-  autoReset: bool.isRequired,
-  onChange: func.isRequired,
-  onBlur: func.isRequired,
-  onFocus: func.isRequired,
-  onKeyPress: func.isRequired,
+    defaultValue: number,
+    value: number,
+    max: number,
+    component: node.isRequired,
+    currency: string.isRequired,
+    config: shape().isRequired,
+    autoFocus: bool.isRequired,
+    autoSelect: bool.isRequired,
+    autoReset: bool.isRequired,
+    onChange: func.isRequired,
+    onBlur: func.isRequired,
+    onFocus: func.isRequired,
+    onKeyPress: func.isRequired,
 };
 
 IntlCurrencyInput.defaultProps = {
-  component: 'input',
-  currency: 'USD',
-  value: 0,
-  config: defaultConfig,
-  autoFocus: false,
-  autoSelect: false,
-  autoReset: false,
-  onChange: f => f,
-  onBlur: f => f,
-  onFocus: f => f,
-  onKeyPress: f => f,
+    component: 'input',
+    currency: 'USD',
+    value: 0,
+    config: defaultConfig,
+    autoFocus: false,
+    autoSelect: false,
+    autoReset: false,
+    onChange: f => f,
+    onBlur: f => f,
+    onFocus: f => f,
+    onKeyPress: f => f,
 };
 
 export default IntlCurrencyInput;
